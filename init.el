c85853a781cd8905065d49f017a96540d9f1a8c2
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-03-29 02:14:28 -0400

    Added init.el

---
+++ init.el
@@ -0,0 +1,8 @@
+;;; Load the config
+(setq emacs-configs-repo "~/Documents/GitHub/emacs-configs")
+
+;; (load-file (concat emacs-config-repo "/babel-config.el")
+;; (load-file (concat emacs-config-repo "/org-export-config.el")
+;; (load-file (concat emacs-config-repo "/basic-configs.el")
+;; (load-file (concat emacs-config-repo "/helpers.el")
+;; (load-file (concat emacs-config-repo "/evil-config.el")
