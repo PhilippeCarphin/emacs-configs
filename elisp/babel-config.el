1b426fdfbe01866ae001787bb77663dd0149062c
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-04-08 02:07:56 -0400

    Set Python3 as interpreter

--- elisp/babel-config.el
+++ elisp/babel-config.el
@@ -1,5 +1,13 @@
-(setq org-confirm-babel-evaluate nil)
+;; Activate languages for executing source blocks
 (org-babel-do-load-languages 'org-babel-load-languages
 			     '((shell . t)
 			       (python . t)))
-(setq org-export-use-babel nil)
+
+;; Get rid of the prompt that asks us if we want to evaluate code blocks
+(setq org-confirm-babel-evaluate nil)
+
+;; Disable automatic execution when exporting
+;; (setq org-export-use-babel nil)
+
+;; Use python3 as the interpreter for python source blocks
+(custom-set-variables '(org-babel-python-command "python3"))
