948e0f39ec60ef2e0f64539a3b380309e73c7016
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-04-08 20:46:38 -0400

    Trial scripts

---
+++ elisp/emacs_agenda.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+py_follow_links()
+{
+    python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" $1
+}
+
+this_file=$(py_follow_links $0)
+this_dir=$(dirname $this_file)
+
+emacs -q --eval "(progn
+                   (load-theme 'misterioso)
+                   (setq package-user-dir \"${this_dir}/elpa\")
+                   (load-file \"$this_dir/org-agenda-config.el\")
+                   (load-file \"$this_dir/org-agenda-addons.el\")
+                   (org-agenda-list))"
---
+++ elisp/emacs_export_evil.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+py_follow_links()
+{
+    python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" $1
+}
+
+this_file=$(py_follow_links $0)
+this_dir=$(dirname $this_file)
+
+emacs -q --eval "(progn
+                    (load-theme 'misterioso)
+                    (setq package-user-dir \"${this_dir}/elpa\")
+                    (load-file \"$this_dir/evil-config.el\")
+                    (load-file \"$this_dir/org-export-config.el\"))"
