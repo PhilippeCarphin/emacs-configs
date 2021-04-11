51ea7c747549051173c50f6c916fff87459d2d2c
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-04-11 14:58:35 -0400

    Correct variable name

--- bin/ecd.sh
+++ bin/ecd.sh
@@ -9,24 +9,24 @@ this_file=$(py_follow_links $0)
 bin_dir=$(dirname $this_file)
 base_dir=$(cd $bin_dir/.. && pwd)
 
-emacs_user_directory=$(pwd)
+user_emacs_directory=$(pwd)
 
 echo $this_file
 echo $base_dir
 echo "USER_EMACS_DIRECTORY = $user_emacs_directory"
-if ! [ -e $emacs_user_directory/init.el ] ; then
-    echo "ERROR: No init.el in emacs_user_directory ($1 must be the path of an emacs config dir)"
+if ! [ -e $user_emacs_directory/init.el ] ; then
+    echo "ERROR: No init.el in user_emacs_directory ($1 must be the path of an emacs config dir)"
     exit 1
 fi
 
-socket=$(basename $emacs_user_directory)
+socket=$(basename $user_emacs_directory)
 # Intercept for certain first arguments
 case $1 in
     -s)
         emacs --daemon=$socket -q --eval "
-          (setq user-emacs-directory
-             \"$emacs_user_directory/\")
-          " -l $emacs_user_directory/init.el -l $base_dir/configs/evil-config.el
+          (progn (setq user-emacs-directory \"$user_emacs_directory/\")
+                 (tool-bar-mode -1)
+                 (load-file (concat user-emacs-directory \"init.el\")))"
         exit 0
         ;;
 esac
