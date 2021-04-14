1c24759bd7aa1da75b5b6f5810ac68658199ec82
Philippe Carphin <philippe.carphin@polymtl.ca>
2021-04-14 07:53:34 -0400

    Make scratch buffer orgmode for babel config

--- configs/babel-basic/init.el
+++ configs/babel-basic/init.el
@@ -1,5 +1,24 @@
 (setq org-confirm-babel-evaluate nil)
+
 (org-babel-do-load-languages 'org-babel-load-languages
 			     '((shell . t)
 			       (python . t)))
+
 (setq org-export-use-babel nil)
+
+;; Use python3 as the interpreter for python source blocks
+(custom-set-variables '(org-babel-python-command "python3"))
+
+(setq initial-major-mode 'org-mode)
+
+(setq initial-scratch-message "\
+#+TITLE: Scratch Buffer
+This buffer is for notes you don't want to save, in orgmode
+If you want to create a file, visit that file with C-x C-f,
+then enter the text in that file's own buffer.
+
+#+begin_src emacs-lisp
+
+,#+end_src
+")
+
